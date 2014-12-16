c11716d706bba35b4aec1f89e8035df7b8055887
Dimitar Dimitrov <dimitar.dimitrov@students.olin.edu>
2014-12-15 19:48:28 -0500

    mem.dat should be legit

--- images/split.py
+++ images/split.py
@@ -1,8 +1,8 @@
 f=open('memh.dat','r')
-g=open('memh3.dat','w')
+g=open('mem.dat','w')
 for line in f:
-    line=(5-len(line))*"0"+line
-    g.write('0x0'+line[0:2]+',\n')
+    line='0x'+(7-len(line))*"0"+line[2:]
     g.write('0x0'+line[2:4]+',\n')
+    g.write('0x0'+line[4:6]+',\n')
 f.close()
 g.close()
